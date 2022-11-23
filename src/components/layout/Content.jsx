2ff8ed03490eb1fd33f50000c021d09fd13b7218
Adriano Fonseca <adrianofonseca@MacBook-Pro-de-Adriano.local>
2022-11-23 19:41:54 -0300

    incluido BrowserRouter e Link

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -10,9 +10,9 @@ import About from "../../views/examples/About";
 const Content = props => (
     <main className="Content">
         <Routes>
+            <Route path="/" exact element={<Home />} />
             <Route path="/about" element={<About />} />
             {/* <Route path="/param/:id" element={<Param />} /> */}
-            <Route path="/" exact element={<Home />} />
             {/* <Route path="*" element={<NotFound />} /> */}
         </Routes>
     </main>
